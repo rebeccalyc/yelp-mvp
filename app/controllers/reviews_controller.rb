fc4200439cf55593fc7db6f74f2619afe31d665b
Rebecca Cheung <rebeccalycc@gmail.com>
2021-08-18 14:30:40 +0100

    yelp

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,33 @@
+class ReviewsController < ApplicationController
+  before_action :set_review, only: [:destroy]
+
+  def new
+    @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = Restaurant.find(params[:restaurant_id])
+    if @review.save
+      redirect_to restaurant_path(@review.restaurant)
+    else
+      render :new
+    end
+  end
+
+  def delete
+    @review.destroy
+    redirect_to restaurant_path(@review.restaurant)
+  end
+
+  private
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating, :restaurant_id)
+  end
+end
